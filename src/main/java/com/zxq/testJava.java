f92b7ebc4a02170907745d56473676d3171cd204
zengxiaoqiang <1229954332@qq.com>
2022-09-07 18:06:31 +0800

    11

---
+++ src/main/java/com/zxq/testJava.java
@@ -0,0 +1,13 @@
+package com.zxq;
+
+/**
+ * 11
+ *
+ * @author zengxiaoqiang
+ * 2022/9/7
+ */
+public class testJava {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
