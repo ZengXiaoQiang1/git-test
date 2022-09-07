4458036832594e52d8305a81e0f2c4a06c4a78b6
zengxiaoqiang <1229954332@qq.com>
2022-09-07 21:02:01 +0800

    master commit

--- src/main/java/com/zxq/testJava.java
+++ src/main/java/com/zxq/testJava.java
@@ -10,5 +10,6 @@ public class testJava {
     public static void main(String[] args) {
         System.out.println("hello,git");
         System.out.println("xxxx");
+        System.out.println("master test");
     }
 }
